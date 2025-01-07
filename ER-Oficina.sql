e848dac712eb889740047c6caada0ea09d268e62
Diegormartins84 <diegormartins84@gmail.com>
2025-01-06 22:21:52 -0300

    Create ER-Oficina.sql

---
+++ ER-Oficina.sql
@@ -0,0 +1,18 @@
+-- Desafio Oficina
+/*
+1 - Mapeamento do esquema ER para o Relacional
+2 - Definição do Script SQL para criação do esquema do banco de dados
+3 - Persistência de dados para testes
+4 - Recuperação de informações com queries SQL
+
+-- Diretrizes
+- Não há um mínimo de queries a serem realizadas
+- Os tópicos supracitados devem estar presentes nas queries
+- Elabore perguntas que podem ser respondidas pelas consultas
+- As cláusulas podem estar presentes em mais de uma query
+*/
+-- drop database oficina_bd;
+show databases;
+create database if not exists oficina_bd;
+use oficina_bd;
+
